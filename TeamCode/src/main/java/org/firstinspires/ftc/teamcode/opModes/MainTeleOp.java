4b23df6bb611a315d3729ec489b2ac4b3af35ebd
MrChips10 <jason.fogiel@outlook.com>
2026-07-13 09:48:08 -0400

    Adds Drive Subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainTeleOp.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
-public class MainTeleOp {
+import static org.firstinspires.ftc.teamcode.subsystem.Drive.DRIVE;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+@TeleOp(name="Test", group = "OpMode")
+public class MainTeleOp extends NextFTCOpMode {
+    public MainTeleOp() {
+        addComponents(
+                new SubsystemComponent(
+                        DRIVE
+                ),
+                BindingsComponent.INSTANCE,
+                BulkReadComponent.INSTANCE
+
+        );
+    }
+    @Override
+    public void onStartButtonPressed() {
+        DRIVE.teleDrive.schedule();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
-public class Drive {
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.hardware.driving.DifferentialArcadeDriverControlled;
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class Drive implements Subsystem {
+    public static Drive DRIVE = new Drive();
+    private Drive() {}
+
+    private final MotorEx leftDrive = new MotorEx("leftDrive");
+    private final MotorEx rightDrive = new MotorEx("rightDrive").reversed();
+
+    public Command teleDrive = new Command() {
+        final DifferentialArcadeDriverControlled opDrive = new DifferentialArcadeDriverControlled(leftDrive, rightDrive, Gamepads.gamepad1().leftStickY(), Gamepads.gamepad1().rightStickX());
+        @Override
+        public void start() {
+            opDrive.start();
+        }
+        @Override
+        public void update() {
+            opDrive.update();
+        }
+        @Override
+        public boolean isDone(){
+            return opDrive.isDone();
+        }
+    };
 }
