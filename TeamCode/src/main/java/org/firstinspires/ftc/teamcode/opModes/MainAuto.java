1cf38d66a8b75e0e4728a616e1dcb53a94fd3da5
MrChips10 <jason.fogiel@outlook.com>
2026-07-14 18:30:55 -0400

    Updates Intake and Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainAuto.java
@@ -1,4 +1,45 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
-public class MainAuto {
+import static org.firstinspires.ftc.teamcode.subsystem.Drive.DRIVE;
+import static org.firstinspires.ftc.teamcode.subsystem.Intake.INTAKE;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.skeletonarmy.marrow.settings.Settings;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import org.firstinspires.ftc.teamcode.marrow.SettingsSetter;
+
+@Autonomous(name="MainAuto", group = "OpMode")
+public class MainAuto extends NextFTCOpMode {
+    public MainAuto() {
+        addComponents(
+                new SubsystemComponent(
+                        DRIVE,
+                        INTAKE
+                ),
+                BulkReadComponent.INSTANCE
+        );
+    }
+
+    @Override
+    public void onInit() {
+        DRIVE.setHeading(90);
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        new SequentialGroup(
+                DRIVE.driveInches(12),
+                DRIVE.turnToAngle(mirrorAngle(180)),
+                DRIVE.turnToAngle(mirrorAngle(90))
+        ).schedule();
+    }
+    private double mirrorAngle(double angle) {
+        if (Settings.get("alliance", SettingsSetter.alliance.RED) == SettingsSetter.alliance.BLUE) {
+            return 180 - angle;
+        }
+        return angle;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
@@ -1,22 +1,53 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.skeletonarmy.marrow.settings.Settings;
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
+import dev.nextftc.control.feedback.AngleType;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.hardware.driving.DifferentialArcadeDriverControlled;
+import dev.nextftc.hardware.impl.Direction;
+import dev.nextftc.hardware.impl.IMUEx;
 import dev.nextftc.hardware.impl.MotorEx;
+import static java.lang.Math.PI;
 
 public class Drive implements Subsystem {
     public static Drive DRIVE = new Drive();
     private Drive() {}
 
+    public enum DriveState {
+        IDLE,
+        TELEOP,
+        DRIVING_INCHES,
+        TURNING_DEGREES
+    }
+    private DriveState state = DriveState.IDLE;
+    private double headingOffset = 90.0;
+
     private final MotorEx leftDrive = new MotorEx("leftDrive");
     private final MotorEx rightDrive = new MotorEx("rightDrive").reversed();
+    private final IMUEx imu = new IMUEx("imu", Direction.UP, Direction.FORWARD);
+
+    private static final double TICKS_PER_REV = 537.7;
+    private static final double WHEEL_DIAMETER_INCHES = 96.0 / 25.4;
+    private static final double TICKS_PER_INCH = TICKS_PER_REV / (WHEEL_DIAMETER_INCHES * PI);
+
+    public double getHeading() {
+        return imu.get().inDeg + headingOffset;
+    }
+    public void setHeading(double degrees) {
+        imu.zero();
+        headingOffset = degrees;
+    }
 
     public Command teleDrive = new Command() {
         final DifferentialArcadeDriverControlled opDrive = new DifferentialArcadeDriverControlled(leftDrive, rightDrive, Gamepads.gamepad1().leftStickY(), Gamepads.gamepad1().rightStickX());
         @Override
         public void start() {
+            state = DriveState.TELEOP;
             opDrive.start();
         }
         @Override
@@ -24,8 +55,91 @@ public class Drive implements Subsystem {
             opDrive.update();
         }
         @Override
+        public void stop(boolean interrupted) {
+            state = DriveState.IDLE;
+        }
+        @Override
         public boolean isDone(){
             return opDrive.isDone();
         }
-    };
+    }.requires(this);
+    public Command driveInches(double inches) {
+        return new Command() {
+            final ControlSystem control = ControlSystem.builder()
+                    .posPid(0.05, 0.0, 0.01)
+                    .build();
+
+            @Override
+            public void start() {
+                state = DriveState.DRIVING_INCHES;
+                leftDrive.setCurrentPosition(0);
+                rightDrive.setCurrentPosition(0);
+                control.setGoal(new KineticState(inches * TICKS_PER_INCH, 0, 0));
+            }
+
+            @Override
+            public void update() {
+                double currentTicks = (leftDrive.getCurrentPosition() + rightDrive.getCurrentPosition()) / 2.0;
+                double power = control.calculate(new KineticState(currentTicks, 0, 0));
+                leftDrive.setPower(power);
+                rightDrive.setPower(power);
+            }
+
+            @Override
+            public boolean isDone() {
+                return control.isWithinTolerance(new KineticState(10, 5, 0));
+            }
+
+            @Override
+            public void stop(boolean interrupted) {
+                state = DriveState.IDLE;
+                leftDrive.setPower(0);
+                rightDrive.setPower(0);
+            }
+        }.requires(this);
+    }
+    public Command turnToAngle(double degrees) {
+        return new Command() {
+            final ControlSystem control = ControlSystem.builder()
+                    .angular(AngleType.DEGREES, b -> b.posPid(0.01, 0, 0.001))
+                    .build();
+
+            @Override
+            public void start() {
+                state = DriveState.TURNING_DEGREES;
+                control.setGoal(new KineticState(degrees, 0, 0));
+            }
+
+            @Override
+            public void update() {
+                double currentAngle = getHeading();
+                double power = control.calculate(new KineticState(currentAngle, 0, 0));
+                leftDrive.setPower(-power);
+                rightDrive.setPower(power);
+            }
+
+            @Override
+            public boolean isDone() {
+                return control.isWithinTolerance(new KineticState(1.0, 0.5, 0));
+            }
+
+            @Override
+            public void stop(boolean interrupted) {
+                state = DriveState.IDLE;
+                leftDrive.setPower(0);
+                rightDrive.setPower(0);
+            }
+        }.requires(this);
+    }
+    
+    @Override
+    public void periodic() {
+        ActiveOpMode.telemetry().addData("Drive State", state);
+        if (Settings.get("debug_mode", false)) {
+            ActiveOpMode.telemetry().addData("Left Ticks", leftDrive.getCurrentPosition());
+            ActiveOpMode.telemetry().addData("Right Ticks", rightDrive.getCurrentPosition());
+            ActiveOpMode.telemetry().addData("Heading", getHeading());
+        }
+        ActiveOpMode.telemetry().update();
+    }
 }
